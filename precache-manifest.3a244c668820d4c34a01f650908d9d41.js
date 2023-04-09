5761406048ba4a4b1e5186285e1389609c604875
PashaLopatuk <60324515+PashaLopatuk@users.noreply.github.com>
2023-04-09 22:28:44 +0300

    Updates

---
+++ precache-manifest.3a244c668820d4c34a01f650908d9d41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7c1ac21ffc8c674fa1b",
+    "url": "/react_weather/static/css/main.f6ff64b7.chunk.css"
+  },
+  {
+    "revision": "c7c1ac21ffc8c674fa1b",
+    "url": "/react_weather/static/js/main.c7c1ac21.chunk.js"
+  },
+  {
+    "revision": "97655c6ba62f874dcb6a",
+    "url": "/react_weather/static/js/1.97655c6b.chunk.js"
+  },
+  {
+    "revision": "c6c1cbe1b507bf5f3994",
+    "url": "/react_weather/static/js/runtime~main.c6c1cbe1.js"
+  },
+  {
+    "revision": "2602a9de58e873af118d712999877bd7",
+    "url": "/react_weather/static/media/cloud.2602a9de.png"
+  },
+  {
+    "revision": "935b11071b35b580cd1928450036c3b2",
+    "url": "/react_weather/index.html"
+  }
+];
